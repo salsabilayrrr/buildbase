cdaf8f91cd5b416266b4ab793e73cd8c41803c35
salsabilayrrr <salsabilayufliramadhani15@gmail.com>
2026-02-24 09:02:36 +0700

    dashboard manager

--- dashboard/manager.php
+++ dashboard/manager.php
@@ -14,46 +14,46 @@ if(!isset($_SESSION['user']) || $_SESSION['user']['role'] != 'manager'){
     <title>Dashboard Manajemen</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&family=Pacifico&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style_manajemen.css">
+    <link rel="stylesheet" href="../assets/css/styleDashboardManager.css">
 </head>
 <body>
 
-    <header class="dashboard-header">
-        <div class="header-left">
-            <img src="../assets/img/logo.png" class="header-logo" alt="Logo">
-            <span class="header-title">Dashboard</span>
-        </div>
-        <a href="logout.php" class="btn-logout-pill">
-            <div class="logout-icon-box">
-                <img src="../assets/img/logout_icon.png" alt="Logout">
+<div class="dashboard-header">
+            <div class="header-left">
+                <img src="../assets/img/logo.png" class="header-logo">
+                <span class="header-title">Dashboard</span>
             </div>
-            <span class="logout-text">Logout</span>
-        </a>
-    </header>
+            <a href="../logout.php" class="btn-logout-pill">
+                <div class="logout-icon-box">
+                    <img src="../assets/img/logout.png" alt="icon">
+                </div>
+                <span class="logout-text">Logout</span>
+            </a>
+        </div>
 
     <main class="scroll-content">
         <div class="container py-4 text-center">
             <h1 class="greeting-text">Halo Manajemen</h1>
             
-            <div class="char-icon-box">
-                <img src="../assets/img/man_character.png" alt="Man Character" class="char-image">
-            </div>
-
-            <div class="summary-label">
+            <div class="summary-section">
+            <img src="../assets/img/avatar.png" alt="Avatar" class="avatar-3d">
+            <div class="summary-pill">
                 Rangkuman Aktivitas Hari Ini :
             </div>
+        </div>
+        </div>
 
             <div class="row g-3 justify-content-center mb-4">
                 <div class="col-5">
                     <div class="stat-card">
-                        <img src="../assets/img/icon_rfq.png" class="stat-icon">
+                        <img src="../assets/img/dok.png" class="stat-icon">
                         <p class="stat-text">RFQ yang Masuk</p>
                         <p class="stat-number">5</p>
                     </div>
                 </div>
                 <div class="col-5">
                     <div class="stat-card">
-                        <img src="../assets/img/icon_project.png" class="stat-icon">
+                        <img src="../assets/img/proyek.png" class="stat-icon">
                         <p class="stat-text">Proyek yang Sedang Dikerjakan</p>
                         <p class="stat-number">4</p>
                     </div>
@@ -78,18 +78,18 @@ if(!isset($_SESSION['user']) || $_SESSION['user']['role'] != 'manager'){
 
     <nav class="bottom-nav">
         <div class="nav-item">
-            <img src="../assets/img/icon_doc.png" class="nav-icon-side">
+            <img src="../assets/img/berkas.png" class="nav-icon-side">
         </div>
         <div class="nav-item">
-            <img src="../assets/img/icon_chat.png" class="nav-icon-side">
+            <img src="../assets/img/arsip.png" class="nav-icon-side">
         </div>
         <div class="nav-item">
             <div class="home-circle">
-                <img src="../assets/img/icon_home.png" class="nav-icon-home">
+                <img src="../assets/img/home.png" class="nav-icon-home">
             </div>
         </div>
         <div class="nav-item">
-            <img src="../assets/img/icon_user.png" class="nav-icon-side">
+            <img src="../assets/img/akun.png" class="nav-icon-side">
         </div>
     </nav>
 
